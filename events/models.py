2e74588e6434c1a874348917a648555389ed370e
Kvstkk <katyav120064@gmail.com>
2025-11-01 18:20:28 +0200

    model and serializer

--- events/models.py
+++ events/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=32)
+    description = models.TextField(blank=True, null=True)
+    appr_minutes_todo = models.IntegerField(default=15)
+    importance = models.PositiveIntegerField(default=1)
+    start_at = models.DateTimeField()
+    end_at = models.DateTimeField()
+    notify_before_min = models.IntegerField(default=15)
+
+    def __str__(self):
+        return self.title
