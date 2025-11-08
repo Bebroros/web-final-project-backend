c6a08f965e9e1152bb6f34c5d98add69cb4f26c8
Bebroros <retrosab@gmail.com>
2025-11-08 16:27:43 +0200

    Add user registration

---
+++ user_auth/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from user_auth.models import CustomUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'date', 'password', 'password2')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'password2': {'write_only': True},
+            'email': {'required': True},
+            'first_name': {'required': False, 'allow_null': True},
+            'second_name': {'required': False, 'allow_null': True},
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        if CustomUser.objects.filter(email=attrs['email']).exists():
+            raise serializers.ValidationError({"email": "Email address already in use."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2', None)
+        password = validated_data.pop('password')
+
+        user = CustomUser(**validated_data)
+        user.set_password(password)
+
+        user.save()
+        return user
