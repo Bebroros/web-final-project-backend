9b2729f7a66915d43def4e6064dcb1c9d29c4ee8
Bebroros <retrosab@gmail.com>
2025-11-08 15:56:24 +0200

    Create custom user model

--- prcalendar/settings.py
+++ prcalendar/settings.py
@@ -83,6 +83,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'user_auth.CustomUser'
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
